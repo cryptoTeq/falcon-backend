6e89480ac97dd96ec22ada91702cb274615cbc03
Amir Zad <ecareteq.com@gmail.com>
2021-06-07 18:25:03 -0400

    deploy to gcp

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from './pipes/validation.pipe';
 
+const port = process.env['PORT'] || 3000;
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(80);
+  await app.listen(port);
 }
 bootstrap();
